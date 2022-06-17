bb4f9edea8c419b6ddaa8e89fb7ddf76734aebdb
Marius-PruteanOSF <marius.prutean@osf.digital>
2022-06-17 10:03:33 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Component} from 'react'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monsters : []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      //.then(users => console.log(users));
+      .then(users => this.setState({monsters: users}));
+      //OR
+      // .then(users => this.setState(
+      //   () => {
+      //     return {monsters: users};
+      //   }, 
+      //   () => {
+      //     console.log(this.state);
+      //   }
+      // ))
+  }
+
+  render () {
+    const {monsters} = this.state;
+    return (
+      <div className="App">
+        {monsters.map(monster => 
+          <div>
+            <h1 key={monsters.id}>{monster.name}</h1>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
